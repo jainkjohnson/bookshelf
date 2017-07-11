249540354d771951e1a77e0055d6bb7024cee9f5
yadhu <yadhu@tuta.io>
2017-07-11 12:56:51 +0530

    encrypt password using bcrypt

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -22,6 +23,19 @@ var UserSchema = new mongoose.Schema({
     toBuy: Array,
     toRead: Array
   },
+});
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+
+    user.password = hash;
+    next();
+  })
 });
 
 module.exports = mongoose.model('User', UserSchema);
